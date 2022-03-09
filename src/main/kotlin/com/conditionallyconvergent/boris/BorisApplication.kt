11d1470f8afbb9c525cc667ccacdc8f9eaba8c51
alexbasson <alexbasson@gmail.com>
2022-03-09 16:32:01 -0500

    Initial commit

---
+++ src/main/kotlin/com/conditionallyconvergent/boris/BorisApplication.kt
@@ -0,0 +1,11 @@
+package com.conditionallyconvergent.boris
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BorisApplication
+
+fun main(args: Array<String>) {
+	runApplication<BorisApplication>(*args)
+}
---
+++ src/test/kotlin/com/conditionallyconvergent/boris/BorisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.conditionallyconvergent.boris
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BorisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
